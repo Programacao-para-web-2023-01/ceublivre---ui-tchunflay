691b3ca171db844941f6eeb9b5df10dd8d3437e0
annarafafo <annarafafo@gmail.com>
2023-06-09 16:38:29 -0300

    adjusted style

--- ceublivre--ui-tchunflay/app/components/footer.tsx
+++ ceublivre--ui-tchunflay/app/components/footer.tsx
@@ -1,36 +1,36 @@
 export default function Footer() {
   return (
-    <div className="bg-store-purple w-full px-24 py-12 flex flex-row gap-6 text-[white] align">
+    <div className="bg-store-purple w-full px-24 py-12 flex flex-row gap-6 text-[white]">
       <div className="flex flex-col justify-around">
-        <a href={"/"} className="text-white">
+        <a href={"/"} className="hover:underline">
           Sobre nós
         </a>
-        <a href={"/"} className="text-white">
+        <a href={"/"} className="hover:underline">
           Comprar
         </a>
-        <a href={"/"} className="text-white">
+        <a href={"/"} className="hover:underline">
           Ajuda
         </a>
       </div>
       <div className="flex flex-col">
-        <a href={"/"} className="text-white">
+        <a href={"/"} className="hover:underline">
           Comunidade
         </a>
-        <a href={"/"} className="red">
+        <a href={"/"} className="hover:underline">
           Contato
         </a>
-        <a href={"/"} className="text-white-200">
+        <a href={"/"} className="hover:underline">
           Redes Sociais
         </a>
       </div>
       <div className="flex flex-col">
-        <a href={"/"} className="text-white">
+        <a href={"/"} className="hover:underline">
           Ferramentas
         </a>
-        <a href={"/"} className="text-white">
+        <a href={"/"} className="hover:underline">
           Notícias
         </a>
-        <a href={"/"} className="text-white">
+        <a href={"/"} className="hover:underline">
           Trabalhe Conosco
         </a>
       </div>
